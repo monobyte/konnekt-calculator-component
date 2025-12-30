3ad2c54a9cbc585590809d22573f6a485113fc81
Dan Carter <monobyte@users.noreply.github.com>
2025-12-30 23:09:12 +0000

    updated build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.ts'),
+      name: 'Calculator',
+      fileName: (format) => `calculator.${format}.js`,
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
